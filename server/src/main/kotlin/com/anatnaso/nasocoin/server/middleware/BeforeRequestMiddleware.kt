439120f7af3a98c79ea08e4030567cf6c600c924
Anothernaso <antondev@kuthy.com>
2025-07-24 11:59:35 +0200

    Fixed logging again

--- server/src/main/kotlin/com/anatnaso/nasocoin/server/middleware/BeforeRequestMiddleware.kt
+++ server/src/main/kotlin/com/anatnaso/nasocoin/server/middleware/BeforeRequestMiddleware.kt
@@ -4,7 +4,7 @@ import io.javalin.http.Context
 import org.slf4j.LoggerFactory
 
 object BeforeRequestMiddleware {
-    private val logger = LoggerFactory.getLogger(AfterRequestMiddleware::class.java)
+    private val logger = LoggerFactory.getLogger(BeforeRequestMiddleware::class.java)
 
     fun beforeRequestHandler(ctx: Context) {
         logger.info("Incoming request at '${ctx.url()}': ${ctx.body()}")

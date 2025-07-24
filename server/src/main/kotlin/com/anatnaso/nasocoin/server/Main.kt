d3bde7eb367050e92a76e6d41b4330c63646b5a7
Anothernaso <antondev@kuthy.com>
2025-07-24 11:41:12 +0200

    Added commented import

--- server/src/main/kotlin/com/anatnaso/nasocoin/server/Main.kt
+++ server/src/main/kotlin/com/anatnaso/nasocoin/server/Main.kt
@@ -2,6 +2,7 @@ package com.anatnaso.nasocoin.server
 
 import com.anatnaso.nasocoin.server.config.ConfigurationManager
 import com.anatnaso.nasocoin.server.database.DatabaseManager
+//import com.anatnaso.nasocoin.server.misc.DatabaseDummy
 import io.javalin.Javalin
 
 fun main() {
